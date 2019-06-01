1bc1846141843bb831177643ed6fc888c02c052a
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:25:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,3 @@
-require 'pry'
-def reverse_each_word(arg)
-  result = []
-  my_arr = arg.split
-  my_arr.each do |word|
-    result << word.reverse
-  end
-  result.join(" ")
-end
-
-
 # def reverse_each_word(arg)
 #   my_arr = arg.split
 #   result = my_arr.collect { |n| n.reverse }
