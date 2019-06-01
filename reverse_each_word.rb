fda77db62e46cc797871242b4a40a95ed0e87578
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:11:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(arg)
   result = []
   my_arr = arg.split
-  my_arr.each do |word|
-    result << word.reverse
-  end
+  # my_arr.each do |word|
+  #   result << word.reverse
+  # end
   result.join(" ")
   
 end
