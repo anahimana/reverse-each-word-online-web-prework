eb0a5dbe39cd0e80264bd64dc397fd8d481b3b50
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:24:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -21,7 +21,7 @@ require 'pry'
 def reverse_each_word(arg)
   my_arr = arg.split
   result = my_arr.collect { |n| n.reverse }
-  result.join (" "
+  result.join (" ")
 end
 
 puts reverse_each_word("Hello there, and how are you?")
