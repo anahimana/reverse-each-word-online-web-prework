c610e6e6f7fcab246ab62522c045c56beb5cf734
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-01 15:20:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,21 @@
-def reverse_each_word(arg)
-  # result = []
-  my_arr = arg.split
-  my_arr.collect_concat do |word|
-    result << word.reverse
-  end
+# def reverse_each_word(arg)
+#   # result = []
+#   my_arr = arg.split
+#   my_arr.collect_concat do |word|
+#     result << word.reverse
+#   end
 
-  # my_arr.each do |word|
-  #   result << word.reverse
-  # end
-  # my_arr.join(" ")
-  # my_arr
-end
+#   # my_arr.each do |word|
+#   #   result << word.reverse
+#   # end
+#   # my_arr.join(" ")
+#   # my_arr
+# end
 
 reverse_each_word("Hello there, and how are you?")
 
 # nums.collect { |n| n * n }
+
+
+def reverse_each_word(arg)
+end
